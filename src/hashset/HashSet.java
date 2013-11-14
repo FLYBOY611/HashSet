8f8fa9ed916ddc025a0389482a2b19ce4b9eaf32
richard.eaton <richard.eaton@ELL000252.ellumen.local>
2013-11-14 11:35:00 -0500

    The next lines

---
+++ src/hashset/HashSet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashset;
+
+/**
+ *
+ * @author richard.eaton
+ */
+public class HashSet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
